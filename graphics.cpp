e97ae3c5585a4d09378462adc2df9d07a097fac1
mccartnp <nicholasmccarty252@gmail.com>
2023-11-17 20:04:31 -0500

    Implemented dynamic color change for the sprite. Next up: introducing a speech bubble for the image (in progress)

--- graphics.cpp
+++ graphics.cpp
@@ -3,8 +3,44 @@
  
 
 
+
+
+
+
 /**
- * Function theMagic: In this functionw we will use SFML methods to 
+ * Function drawSpeechBubble: Draws a speech bubble on the given window.
+ * Params: sf::RenderWindow& window - the window to draw on
+ *         sf::Vector2f position - the position of the speech bubble
+ *         std::string text - the text to display in the speech bubble
+ * return : void
+*/
+void drawSpeechBubble(sf::RenderWindow& window, sf::Vector2f position, std::string text) {
+    sf::RectangleShape bubble;
+    bubble.setSize(sf::Vector2f(200.0f, 100.0f)); // Adjust the size of the speech bubble as needed
+    bubble.setFillColor(sf::Color::White);
+    bubble.setOutlineThickness(2);
+    bubble.setOutlineColor(sf::Color::Black);
+    bubble.setPosition(position.x - bubble.getSize().x / 2.0f, position.y - bubble.getSize().y / 2.0f);
+
+    sf::Text bubbleText;
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+        std::cerr << "Failed to load font." << std::endl;
+        // Handle the error appropriately, e.g., return from the function or exit the program.
+    }
+    bubbleText.setFont(font);
+    bubbleText.setString(text);
+    bubbleText.setCharacterSize(16);
+    bubbleText.setFillColor(sf::Color::Black);
+    bubbleText.setPosition(position.x - bubbleText.getLocalBounds().width / 2.0f,
+                            position.y - bubbleText.getLocalBounds().height / 2.0f);
+
+    window.draw(bubble);
+    window.draw(bubbleText);
+}
+
+/**
+ * Function theMagic: In this function we will use SFML methods to 
  * create our logo and animate it.
  * Params: none
  * return : void
@@ -15,7 +51,6 @@ void theMagic() {
     // TODO - Put the image you want here. In my example, I will use the Xavier Musketeers basketball logo.
 
     const std::string imagePath = "xavier.png";
-
 
 
     // Load the image
@@ -30,6 +65,12 @@ void theMagic() {
 
     // Create a sprite and set its texture
     sf::Sprite sprite(texture);
+    sf::Time elapsed;
+    sf::Clock clock;
+    
+    drawSpeechBubble(window, sf::Vector2f(25, 25), "Hello, SFML!");
+    
+
 
     // Creating a position vector and setting the initial velocity of it.
     sf::Vector2f position(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
@@ -55,6 +96,23 @@ void theMagic() {
         if (position.y < 0 || position.y > window.getSize().y - sprite.getLocalBounds().height) {
             velocity.y = -velocity.y;
         }
+
+        elapsed = clock.getElapsedTime();
+
+        // Change the color every 1 second
+        if (elapsed.asSeconds() >= 1.0f) {
+            // Toggle between red and blue
+            if (sprite.getColor() == sf::Color::Red) {
+                sprite.setColor(sf::Color::Blue);
+            } else {
+                sprite.setColor(sf::Color::Red);
+            }
+
+            // Reset the clock
+            clock.restart();
+        }
+
+
         // Clear the window
         window.clear();
 
